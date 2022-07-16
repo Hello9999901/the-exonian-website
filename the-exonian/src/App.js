466c9904ed148cd962d059f2d5c20abfa4263798
Hello9999901 <byran8855@gmail.com>
2022-07-16 15:04:31 -0400

    working on nav + react setup

--- the-exonian/src/App.js
+++ the-exonian/src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import './index.css';
+import logo from './img/exonian-logo.png';
+
+function Nav() {
+	return (
+		<div>
+			<div className="flex flex-row justify-center py-4">
+				<img src={logo} alt="Logo" className="w-1/5" />
+			</div>
+			<hr className="border-gray-300"></hr>
+			<div className="flex flex-row justify-center py-2 font-normal text-sm">
+				<a href="https://www.google.com" className="px-5">About</a>
+				<a href="https://www.google.com" className="px-5">News</a>
+				<a href="https://www.google.com" className="px-5">Life</a>
+				<a href="https://www.google.com" className="px-5">Opinions</a>
+				<a href="https://www.google.com" className="px-5">Sports</a>
+				<a href="https://www.google.com" className="px-5">Humor</a>
+				<a href="https://www.google.com" className="px-5">Archives</a>
+				<a href="https://www.google.com" className="px-5">Opinions</a>
+			</div>
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="w-screen h-screen">
+			<Nav></Nav>
+		</div>
+	);
 }
 
 export default App;
