fb83b314c97fcb24ac8b329b7e7226a043351eb7
Hello9999901 <byran8855@gmail.com>
2022-07-19 15:01:06 -0400

    ui additions

--- the-exonian/src/App.tsx
+++ the-exonian/src/App.tsx
@@ -17,14 +17,14 @@ function Nav() {
 function Sublinks() {
 	return (
 		<div className="flex flex-row flex-wrap justify-center py-3 font-normal text-sm">
-				<a href="https://www.google.com" className="mx-5">About</a>
-				<a href="https://www.google.com" className="mx-5">News</a>
-				<a href="https://www.google.com" className="mx-5">Life</a>
-				<a href="https://www.google.com" className="mx-5">Opinions</a>
-				<a href="https://www.google.com" className="mx-5">Sports</a>
-				<a href="https://www.google.com" className="mx-5">Humor</a>
-				<a href="https://www.google.com" className="mx-5">Archives</a>
-				<a href="https://www.google.com" className="mx-5">Opinions</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">About</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">News</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">Life</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">Opinions</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">Sports</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">Humor</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">Archives</a>
+				<a href="https://www.google.com" className="mx-5 ul-hover">Opinions</a>
 		</div>
 	);
 }
@@ -34,15 +34,15 @@ function Grid() {
 		<div className="grid gap-4 grid-cols-12 h-fit max-w-[1200px] w-11/12 py-2 relative">
 			<div className="col-start-4 col-end-10 row-start-1 row-end-3">
 				<img src="https://images.unsplash.com/photo-1522202176988-66273c2fd55f?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2071&q=80" alt=""/>
-				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang"></ArticleLink>
+				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang" link="https://www.google.com"></ArticleLink>
 			</div>
 			<div className="col-start-1 col-end-4">
-				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang"></ArticleLink>
-				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang"></ArticleLink>
+				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang" link="https://www.google.com"></ArticleLink>
+				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang" link="https://www.google.com"></ArticleLink>
 			</div>
 			<div className="col-start-10 col-end-13">
-				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang"></ArticleLink>
-				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang"></ArticleLink>
+				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang" link="https://www.google.com"></ArticleLink>
+				<ArticleLink title="When You Realize Talking is More Than Speaking" author="Byran Huang" link="https://www.google.com"></ArticleLink>
 			</div>
 		</div>
 	);
@@ -51,7 +51,7 @@ function Grid() {
 function ArticleLink(input: any) {
 	return (
 		<div>
-			<h1 className="text-4xl font-serif font-semibold">{input.title}</h1>
+			<a href={input.link} className="text-4xl font-serif font-semibold hover:text-neutral-600 ease-in-out duration-200">{input.title}</a>
 			<p>By: {input.author}</p>
 		</div>
 	);
